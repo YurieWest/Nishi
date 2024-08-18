d2906b114f5abc3316097b55b0f7f708efb0d776
YurieYoshimura <yurieyoshimura@gmail.com>
2024-08-18 15:02:00 +0200

    Create work.js

---
+++ work.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".tiptext").mouseover(function() {
+        $(this).children(".description").show();
+    }).mouseout(function() {
+        $(this).children(".description").hide();
+    });
+});
