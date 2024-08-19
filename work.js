23f23f0daa1a8ba4a0b3527debf8d759b8588580
YurieYoshimura <yurieyoshimura@gmail.com>
2024-08-19 16:36:16 +0200

    Update work.js

--- work.js
+++ work.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
-    $(".tiptext").mouseover(function() {
-        $(this).children(".description").show();
-    }).mouseout(function() {
-        $(this).children(".description").hide();
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const introScreen = document.getElementById('intro-screen');
+  const mainContent = document.getElementById('main-content');
+
+  introScreen.addEventListener('animationend', (event) => {
+    if (event.animationName === 'brushStroke') {
+      introScreen.style.display = 'none';
+      mainContent.style.display = 'block';
+      mainContent.style.opacity = '1';
+    }
+  });
 });
