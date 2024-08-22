2d5ef9c65c5c57d3f68a8c9839bffa577a99ed14
YurieYoshimura <yurieyoshimura@gmail.com>
2024-08-22 17:43:53 +0200

    Update work.js

--- work.js
+++ work.js
@@ -1,4 +1,16 @@
+
 document.body.style.cursor = "url('https://raw.githubusercontent.com/YurieYoshimura/west/main/cursor.cur'), auto";
+
+// Add CSS for link hover cursor change
+const style = document.createElement('style');
+style.innerHTML = `
+  a:hover {
+    cursor: url('https://raw.githubusercontent.com/YurieYoshimura/west/main/cursor%20(2).cur'), auto;
+  }
+`;
+document.head.appendChild(style);
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const introScreen = document.getElementById('intro-screen');
   const mainContent = document.getElementById('main-content');
